16b0bb55ef9d183ea2de68a397bfb96e50eb5a26
tuliust <tuliust@gmail.com>
2026-07-06 20:20:20 -0300

    Remove obsolete ticket side-effect import

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './app/App';
 import './styles.css';
-import './lib/ticket-experience';
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
