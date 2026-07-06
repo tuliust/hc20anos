9185cf1f0d921713333ff783c523d9c5672238ea
tuliust <tuliust@gmail.com>
2026-07-06 19:59:27 -0300

    Enable ticket experience enhancements

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './app/App';
 import './styles.css';
+import './lib/ticket-experience';
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
