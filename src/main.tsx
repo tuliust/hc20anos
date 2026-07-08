c2b1b3ba276a6b1af30b5a263453c9954f923673
tuliust <tuliust@gmail.com>
2026-07-08 00:00:29 -0300

    Load profile registration runtime

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,10 @@ import { createRoot } from 'react-dom/client';
 import App from './app/App';
 import './styles.css';
 import './lib/ticket-enhancements-runtime';
+import './lib/profile-registration-runtime';
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
