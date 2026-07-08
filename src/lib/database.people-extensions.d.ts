542fb40202227a3fa6768ca690fd6a534b0737ff
tuliust <tuliust@gmail.com>
2026-07-08 00:02:55 -0300

    Make people verification type fields optional

--- src/lib/database.people-extensions.d.ts
+++ src/lib/database.people-extensions.d.ts
@@ -2,7 +2,7 @@ import "./database.types";
 
 declare module "./database.types" {
   interface DbPerson {
-    birth_year: number | null;
-    verification_status: "not_started" | "in_progress" | "verified" | "failed" | "manual_review" | string | null;
+    birth_year?: number | null;
+    verification_status?: "not_started" | "in_progress" | "verified" | "failed" | "manual_review" | string | null;
   }
 }
