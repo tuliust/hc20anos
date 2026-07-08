82ad011c5b41ec410ec12a74c687a3fbf2f640d8
tuliust <tuliust@gmail.com>
2026-07-08 00:21:03 -0300

    Load header home list runtime

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-﻿import React from 'react';
+import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './app/App';
 import './styles.css';
@@ -6,6 +6,7 @@ import './lib/ticket-enhancements-runtime';
 import './lib/profile-registration-runtime';
 import './lib/edit-profile-runtime';
 import './lib/edit-profile-avatar-runtime';
+import './lib/header-home-list-runtime';
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
