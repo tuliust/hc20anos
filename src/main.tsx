818b93853bff7e1242553925c059b4e857fc330d
tuliust <tuliust@gmail.com>
2026-07-08 00:15:17 -0300

    Load avatar sync runtime

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import './styles.css';
 import './lib/ticket-enhancements-runtime';
 import './lib/profile-registration-runtime';
 import './lib/edit-profile-runtime';
+import './lib/edit-profile-avatar-runtime';
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
