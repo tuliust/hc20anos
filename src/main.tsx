d6c8b27951d25094d85bde28f904b99ca75071b3
tuliust <tuliust@gmail.com>
2026-07-13 13:27:25 -0300

    feat: install neutral CMS defaults before app render

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,10 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './app/App';
 import { AdminCmsPanelsMount } from './app/AdminCmsPanelsMount';
+import { installNeutralCmsDefaults } from './lib/neutralCmsDefaults';
 import './styles.css';
+
+installNeutralCmsDefaults();
 
 const rootElement = document.getElementById('root');
 
