61809d39dde5c7b1fc980c263cfff12641987e95
tuliust <tuliust@gmail.com>
2026-07-27 12:11:45 -0300

    refactor: remover augmentação redundante de pessoa

--- src/lib/database.people-extensions.d.ts
+++
@@ -1,8 +0,0 @@
-import "./database.types";
-
-declare module "./database.types" {
-  interface DbPerson {
-    birth_year?: number | null;
-    verification_status?: "not_started" | "in_progress" | "verified" | "failed" | "manual_review" | string | null;
-  }
-}
